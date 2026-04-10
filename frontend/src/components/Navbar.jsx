d6c67c79943a37fb6d188b93231ffa628a6be075
shabeeb <shabeebputhalath069@gmail.com>
2026-04-10 20:15:48 +0530

    frontend full completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ShopContext } from '../context/ShopContext'
 
 function Navbar() {
     const [visible, setVisible] = useState(false);
-    const {setShowSearch} = React.useContext(ShopContext);
+    const {setShowSearch, getCartCount} = React.useContext(ShopContext);
   return (
     <div className='flex items-center justify-between py-5 font-medium '>
          <Link to='/'><img className='w-36' src={assets.logo} alt="Logo" /></Link>
@@ -31,7 +31,7 @@ function Navbar() {
          <div className='flex items-center gap-6'>
                 <img onClick={() => setShowSearch(true)} src={assets.search_icon} alt="Search"  className='w-5  cursor-pointer'/>
                 <div className='group relative'>
-                  <img src={assets.profile_icon} alt="User"  className='w-5  cursor-pointer'/>
+                  <Link to='/login'><img src={assets.profile_icon} alt="User"  className='w-5  cursor-pointer'/></Link>
                   <div className='group hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                        <p className='cursor-pointer hover:text-black'>My Profile</p>
@@ -42,7 +42,7 @@ function Navbar() {
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} alt="Cart"  className='w-5 min-w-5  cursor-pointer'/>
-                    <p className='absolute bottom-[-5px] right-[-5px]  text-white text-center leading-4 bg-black rounded-full  w-4 aspect-square'>0</p>
+                    <p  className='absolute bottom-[-5px] right-[-5px]  text-white text-center leading-4 bg-black rounded-full  w-4 aspect-square'>{getCartCount()}</p>
                 </Link>
                 <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
          </div>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 import { products } from '../assets/assets';
 import { useState } from 'react';
 import{toast} from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
  export const ShopContext = createContext();
  const ShopContextProvider =(props)=>{
@@ -11,6 +12,7 @@ import{toast} from 'react-toastify';
     const [search,setSearch] =useState('');
     const[showSearch,setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate=useNavigate();
 
 
     const addToCart =async (itemId,size) => {
@@ -37,8 +39,44 @@ import{toast} from 'react-toastify';
         }
         setCartItems(cartData);
     };
+    const getCartCount = () => {
+        let totalCount =0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try{
+                    if (cartItems[items][item]>0) {
+                      totalCount += cartItems[items][item];  
+                    }                    
+                }catch(err){
+                    console.error('Error calculating cart count:', err);
+                }
+            }
+}
+    return totalCount;
+    };
 
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
 
+   const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {
+          // Error handling if needed
+        }
+      }
+    }
+    return totalAmount;
+  }
 
     const value={
         products,
@@ -49,7 +87,11 @@ import{toast} from 'react-toastify';
         showSearch,
         setShowSearch,
         cartItems,
-        addToCart
+        addToCart,
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate
         
     }
     return(
