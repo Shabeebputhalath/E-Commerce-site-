2a6b895a8a1c3fab1745a8ec3c98180dc02ae2a8
muhammedshamilmt <muhammedhishamptb@gmail.com>
2026-04-21 18:16:37 +0530

    admin panel almost files added

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
       
@@ -11,7 +11,12 @@ const Navbar = () => {
         alt="" 
       />
 
-      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>
+      <button 
+        className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'
+        onClick={() => {
+          setToken("")
+        }}
+      >
         Logout
       </button>
 
